9ad783c6b5847d4a71ae446ebddf78511995197a
Maximilian Krupper <149516068+maximiliankrupper@users.noreply.github.com>
2025-11-06 17:04:11 +0100

    Add setup instructions comment in main.jsx
    Added a comment for project setup instructions.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+   {/* so kann es gestartet werden npm create vite@latest mensabot-react --template react*/}
+
 ReactDOM.createRoot(document.getElementById("app")).render(
   <React.StrictMode>
     <App />
