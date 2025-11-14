54a65842c393121198c87e4ecf1a810a13b2daf6
Tobias Veselsky <veselsky@tu-berlin.de>
2025-11-14 10:58:26 +0100

    Just some API testing with SAIA.

--- backend/apps/api_backend/src/mensa_api_backend/main.py
+++ backend/apps/api_backend/src/mensa_api_backend/main.py
@@ -3,8 +3,38 @@ from typing import Optional
 from pydantic import BaseModel
 from openai import OpenAI
 
+api_key = ""
+base_url = "https://chat-ai.academiccloud.de/v1"
+model = "meta-llama-3.1-8b-instruct"
+
 app = FastAPI()
-client = OpenAI()
+client = OpenAI(api_key=api_key, base_url=base_url)
+
+messages=[{"role":"system","content":"You are a helpful assistant"},{"role":"user","content":"You should see a deepwiki MCP tool. Tell me about the mcp tools available to you. Use some of them and tell me about the results."}]
+
+# Test chat completion
+chat_completion = client.chat.completions.create(
+    messages=messages,
+    model= model,
+)
+print(chat_completion)
+
+# Test responses with MCP tool
+resp = client.responses.create(
+    model=model,
+    input=messages,
+    tools=[
+        {
+            "type": "mcp",
+            "server_label": "deepwiki_mcp",
+            "server_url": "https://mcp.deepwiki.com/mcp",
+            "require_approval": "never",
+        }
+    ],
+)
+
+print(resp.output_text)
+print(resp)
 
 MCP_URL = "http://localhost:8000/mcp"
 
