95bf35010684e6764f8da1cc68e60835abf2c64e
asbstu <a.siebing@campus.tu-berlin.de>
2025-12-04 00:29:35 +0100

    chore: documented api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,19 @@
-// API - communication
-// config fetch-function
-// send chat Message
+/**
+ * @file
+ * This is the API client for `/backend/apps/api_backend/src/mensa_api_backend`.
+ * You can work with it directly although it is advised to work with the
+ * storage-integrated higher-level classes from `./chats.ts`.
+ * 
+ * With `chats.ts`, sending a message would look like the following:
+ * ```tsx
+ * const chat = Chats.getById("my-chat");
+ * const response = await chat.send(client, "Hello");
+ * console.log("Message History:", chat.messages);
+ * 
+ * With the raw {@link MensaBotClient}, you would have to manage
+ * storage and sessions yourself.
+ * ```
+ */
 
 import type { ChatMessage } from "./chats";
 
