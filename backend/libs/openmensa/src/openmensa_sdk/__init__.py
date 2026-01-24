db340404b6756f2fc96a32e61d7cc1026948a6e0
Tobias Veselsky <veselsky@tu-berlin.de>
2026-01-24 17:30:09 +0100

    Updated pyproject toml to include rapidfuzz and updated the __init__ file.

--- backend/libs/openmensa/src/openmensa_sdk/__init__.py
+++ backend/libs/openmensa/src/openmensa_sdk/__init__.py
@@ -4,11 +4,17 @@ Author: Tobias Veselsky
 Description: Public imports for OpenMensa SDK package.
 """
 
+from .canteen_index import CanteenIndex, CanteenIndexStore, CanteenSearchResult, DEFAULT_INDEX_PATH, DEFAULT_INDEX_TTL_HOURS
 from .client import OpenMensaClient, DEFAULT_API_URL
 from .errors import OpenMensaAPIError
 from .models import Canteen, Day, Meal, PriceInfo
 
 __all__ = [
+    "CanteenIndex",
+    "CanteenIndexStore",
+    "CanteenSearchResult",
+    "DEFAULT_INDEX_PATH",
+    "DEFAULT_INDEX_TTL_HOURS",
     "OpenMensaClient",
     "OpenMensaAPIError",
     "Canteen",
