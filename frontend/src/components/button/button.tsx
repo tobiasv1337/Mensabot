458818ef0267d509377d163dc4bc0d15ba1ea066
Judith <j.bucher@campus.tu-berlin.de>
2026-01-29 01:27:13 +0100

    Add left sidebar icon and enhance Button component with new styles and props

--- frontend/src/components/button/button.tsx
+++ frontend/src/components/button/button.tsx
@@ -1,7 +1,7 @@
 // The logic and JSX of the component
 import { forwardRef } from 'react';
 import type { ButtonProps } from './button.types';
-import { StyledButton } from './button.styles';
+import { StyledButton, ButtonTextWrapper } from './button.styles';
 
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
@@ -9,6 +9,8 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
         {
             variant = 'default',
             size = 'hug',
+            active = false,
+            collapsed = false,
             iconLeft,
             iconRight,
             text,
@@ -25,12 +27,13 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
                 ref={ref}
                 $variant={variant}
                 $size={size}
+                $active={active}
                 disabled={disabled}
                 {...rest}
             >
                 {/* only renders if there */}
                 {iconLeft && <span className="icon-left">{iconLeft}</span>}
-                {content}
+                {children ? children : <ButtonTextWrapper collapsed={collapsed}>{content}</ButtonTextWrapper>}
                 {iconRight && <span className="icon-right">{iconRight}</span>}
             </StyledButton>
         );
