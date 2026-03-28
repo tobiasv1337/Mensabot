a4ac636b4554ae0365dba9bd9a5d82810d8d9a00
Tobias Veselsky <veselsky@tu-berlin.de>
2026-03-28 19:00:58 +0100

    Feat: add proactive PWA install prompts

--- frontend/src/components/icons.tsx
+++ frontend/src/components/icons.tsx
@@ -95,6 +95,18 @@ export const SettingsIcon = () => (
     </svg>
 );
 
+export const InstallIcon = () => (
+    <svg
+        width="20"
+        height="20"
+        viewBox="0 -960 960 960"
+        fill="currentColor"
+        aria-hidden
+    >
+        <path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z" />
+    </svg>
+);
+
 export const ShortcutsIcon = () => (
     <svg
         width="20"
